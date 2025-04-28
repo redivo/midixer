3746ab999bad02a9b6af4f1289ae32c00281bcba
George Redivo <georgeredivo@gmail.com>
2025-04-28 12:59:47 -0300

    Create a simple track class
    The class has the following attributes:
     - Mute
     - Solo
     - REC Arm
     - Number
     - Fader (volume)
     - Pan
     - Meter
     - Name

--- software/midixer_arduino/midixer_arduino.ino
+++ software/midixer_arduino/midixer_arduino.ino
@@ -7,6 +7,7 @@
 
 #include "MIDIUSB.h"
 #include "midi-msg.h"
+#include "track.h"
 
 void setup()
 {
@@ -15,6 +16,7 @@ void setup()
 
 void loop() {
     midiEventPacket_t rx;
+    midixer::Track track;
 
     do {
         rx = MidiUSB.read();
